9c96ad0924adb4c5222ca7c2cb28e49424d23014
GitHub Action <action@github.com>
2021-09-04 05:28:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/linkser/decompier/MyBundle.kt
+++ src/main/kotlin/com/github/linkser/decompier/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.linkser.decompier
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/linkser/decompier/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linkser/decompier/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linkser.decompier.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linkser.decompier.MyBundle
 
 class MyApplicationService {
 
